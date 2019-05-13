ab65c1860f62218e50abcb190f768c2beae01172
cptdagon <adam.b.rushby@gmail.com>
2019-05-13 17:24:29 +0100

    added second api call

--- Gen_Keyspace_api.py
+++ Gen_Keyspace_api.py
@@ -16,7 +16,21 @@ class ApiPing(Resource):
         second = stdout.decode("utf-8").split('\n')[13]
         return {"storage": first.split(': ')[1],"server":second.split(': ')[1]}
 
+class dataFetch(Resource):
+    def get(self):
+        jsonobject = dict()
+        with GraknClient(uri="localhost:48555") as client:
+            with client.session(keyspace="dev_test") as session:
+                with session.transaction().read() as read_transaction:
+                    match_iterator = read_transaction.query('match $p isa person;get;')
+                    answers = match_iterator.collect_concepts()
+                    for answer in answers:
+                       jsonobject["id"] = answer.id
+                    return {"answers": [ jsonobject ]}
+
 api.add_resource(ApiPing, '/ping')
+api.add_resource(dataFetch, '/fetchpeople')
 
 if __name__ == '__main__':
    app.run(debug=True)
+
