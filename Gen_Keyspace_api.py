c38515d7eabfa6f25f35ca635808d26801efa258
cptdagon <adam.b.rushby@gmail.com>
2019-05-14 10:32:41 +0100

    changing json building from dictionaries to strings for better/easy json building

--- Gen_Keyspace_api.py
+++ Gen_Keyspace_api.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 from grakn.client import GraknClient
 import subprocess
+import json
 
 app = Flask(__name__)
 api = Api(app)
@@ -17,19 +18,20 @@ class ApiPing(Resource):
         return {"storage": first.split(': ')[1],"server":second.split(': ')[1]}
 
 class dataFetch(Resource):
-    def get(self):
-        jsonobject = dict()
+    def get(self, thing):
+        jsonobject = '{"answers": ['
         with GraknClient(uri="localhost:48555") as client:
             with client.session(keyspace="dev_test") as session:
                 with session.transaction().read() as read_transaction:
-                    match_iterator = read_transaction.query('match $p isa person;get;')
+                    match_iterator = read_transaction.query('match $t isa '+thing+';get;')
                     answers = match_iterator.collect_concepts()
                     for answer in answers:
-                       jsonobject["id"] = answer.id
-                    return {"answers": [ jsonobject ]}
+                       jsonobject = jsonobject + json.dumps({"id":answer.id})+','
+                    jsonobject = jsonobject[:-1]+']}'
+                    return json.loads(jsonobject)
 
 api.add_resource(ApiPing, '/ping')
-api.add_resource(dataFetch, '/fetchpeople')
+api.add_resource(dataFetch, '/fetch/<string:thing>')
 
 if __name__ == '__main__':
    app.run(debug=True)
