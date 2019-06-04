171c24d5a79bfb071bd2244d16a8cf4d639864c6
cptdagon <adam.b.rushby@gmail.com>
2019-06-04 11:34:18 +0100

    removed commented out code

--- Gen_Keyspace_api.py
+++ Gen_Keyspace_api.py
@@ -27,85 +27,85 @@ class ApiPing(Resource): #pings grakn server for status.
 
 class builders():
 
+    #### builds entity json ####
     @classmethod
-    def entity_builder(cls,entity): #builds a entity along with its associated attributes, roles, and keys
+    def entity_builder(cls,entity):
         jsonobject = json.dumps({"id":entity.id, "label":entity.type().label()})[:-1]+',"contains":[{ '
-        #jsonobject = jsonobject + cls.attributes_builder(entity.attributes())
-        #jsonobject = jsonobject + cls.roles_builder(entity.roles())
-        #jsonobject = jsonobject + cls.keys_builder(entity.keys())
         jsonobject = jsonobject + ( cls.attributes_builder(entity.attributes()) + cls.roles_builder(entity.roles()) + cls.keys_builder(entity.keys()) )
         jsonobject = jsonobject[:-1]+'}]},'
         return jsonobject
-
+    #### builds a list of entity json ####
     @classmethod
-    def entities_builder(cls,entities): #builds a list of entities
+    def entities_builder(cls,entities):
         jsonobject = ""
         for entity in entities:
             jsonobject = jsonobject + cls.entity_builder(entity)
         jsonobject = jsonobject[:-1]
         return jsonobject
 
+    #### builds attribute json ####
     @classmethod
-    def attribute_builder(cls,attribute): #builds an attribute along with its owned attributes, roles, and keys
+    def attribute_builder(cls,attribute):
         jsonobject = json.dumps({"label":attribute.type().label(),"value":attribute.value()}, default = str)[:-1]+',"contains":[{ '
         jsonobject = jsonobject + cls.attributes_builder(attribute.attributes())
         jsonobject = jsonobject + cls.roles_builder(attribute.roles())[:-1]+'}]},'
         return jsonobject
 
+    #### builds a list of attribute json ####
     @classmethod
-    def attributes_builder(cls,attributes): #builds a list of attributes
+    def attributes_builder(cls,attributes):
         jsonobject = '"attributes":[ '
         for attribute in attributes:
             jsonobject = jsonobject + cls.attribute_builder(attribute)
         jsonobject = jsonobject[:-1]+'],'
         return jsonobject
 
+    #### builds role json ####
     @classmethod
-    def role_builder(cls,role): #builds a role
+    def role_builder(cls,role):
         jsonobject = json.dumps({"label":role.label()}, default = str)+','
         return jsonobject
 
+    #### builds a list of role json ####
     @classmethod
-    def roles_builder(cls,roles): #builds a list of roles
+    def roles_builder(cls,roles):
         jsonobject = '"roles":[ '
         for role in roles:
             jsonobject = jsonobject + cls.role_builder(role)
         jsonobject = jsonobject[:-1]+'],'
         return jsonobject
 
+    #### builds key json ####
     @classmethod
-    def key_builder(cls,key): #builds a key
+    def key_builder(cls,key):
         jsonobject = json.dumps({"label":key.label()},default = str)+','
         return jsonobject
 
+    #### builds a list of key json ####
     @classmethod
-    def keys_builder(cls,keys): #builds a list of keys
+    def keys_builder(cls,keys):
         jsonobject = '"keys":[ '
         for key in keys:
             jsonobject = jsonobject + cls.key_builder(key)
         jsonobject = jsonobject[:-1]+'],'
         return jsonobject
 
+    #### builds player json ####
     @classmethod
     def player_builder(cls,player):
         jsonobject = '{'
         if player.is_attribute():
             jsonobject = jsonobject + '"attribute":[{ ' + cls.attribute_builder(player)[:-1] + '}],"entity":[],"relation":[],'
-            #jsonobject = jsonobject + cls.attribute_builder(player)
-            #jsonobject = jsonobject[:-1] + '}],"entity":[],"relation":[],'
         elif player.is_entity():
             jsonobject = jsonobject + '"attribute":[],"entity":[ ' + cls.entity_builder(player)[:-1] + '],"relation":[]'
-            #jsonobject = jsonobject + cls.entity_builder(player)
-            #jsonobject = jsonobject[:-1] + '],"relation":[]'
         elif player.is_relation():
             jsonobject = jsonobject + '"attribtue":[],"entity":[],"relation":[ ' + cls.relation_builder(player)[:-1] + ']'
-            #jsonobject = jsonobject + cls.relation_builder(player)
-            #jsonobject = jsonobject[:-1] + ']'
         else:
             jsonobject = jsonobject + '"attribute":[],"entity":[],"relation":[]'
         jsonobject = jsonobject + '},'
         return jsonobject
 
+    #### builds a list of player json ####
     @classmethod
     def players_builder(cls,players):
         jsonobject = '"players":[ '
@@ -114,25 +114,24 @@ class builders():
         jsonobject = jsonobject[:-1] + '],'
         return jsonobject
 
+    #### builds relation json ####
     @classmethod
-    def relation_builder(cls,relation): #builds a relation with its players, and owned attributes, roles, and keys
+    def relation_builder(cls,relation):
         jsonobject = json.dumps({"id":relation.id, "label":relation.type().label()})[:-1]+',"contains":[{ '
-        #jsonobject = jsonobject + cls.players_builder(relation.role_players())
-        #jsonobject = jsonobject + cls.attributes_builder(relation.attributes())
-        #jsonobject = jsonobject + cls.roles_builder(relation.roles())
-        #jsonobject = jsonobject + cls.keys_builder(relation.keys())
         jsonobject = jsonobject + ( cls.players_builder(relation.role_players()) + cls.attributes_builder(relation.attributes()) + cls.roles_builder(relation.roles()) + cls.keys_builder(relation.keys()) )
         jsonobject = jsonobject[:-1]+'}]},'
         return jsonobject
 
+    #### builds a list of relation json ####
     @classmethod
-    def relations_builder(cls,relations): #builds a list of relations
+    def relations_builder(cls,relations):
         jsonobject = ""
         for relation in relations:
             jsonobject = jsonobject + cls.relation_builder(relation)
         jsonobject = jsonobject[:-1]
         return jsonobject
 
+    #### determines how to behave based on the contents of the results fetched from grakn ####
     @classmethod
     def objectSwitch(cls,answers):
         a = False
@@ -158,13 +157,15 @@ class builders():
         elif (r and (not (a or e))):
             jsonobject = jsonobject + builders.relations_builder(answers)
         else:
-            #### neither should this ####
+            #### hits this if more than one type is found in the list of answers ####
+            #### typicaly occurs when a get $a, $b; term is used.
             abort(500, "well this is rather awkward... :| ")
         return jsonobject
 
-#######################
-### grakn match api ###
-#######################
+#########################
+#### grakn match api ####
+#########################
+
 class genApiFetch(Resource):  #### basic fetch request ####
     def get(self,
             kspace,       #### mandatory #### keyspace name ####
@@ -173,7 +174,7 @@ class genApiFetch(Resource):  #### basic fetch request ####
                                              #### parameter name => parameter value  => e.g: 'name="Jim"' ####
                                              #### parametre name => parameter variable => e.g: 'name=$n' ####
                                              #### multiple values are comma seperated and types of values can be mixed => e.g: 'name=$n,eyeColor="blue"' ####
-            get = " ",    #### not mandatory #### specify variables to fetch data from => e.g: '$t,$n' ####
+            get = "$t",    #### not mandatory #### specify variables to fetch data from => e.g: '$t,$n' ####
             limit = 100): #### not mandatory #### fetch quantity limit => used to improve response time of api ####
         #### parameters ####
         split = has.split(',')
@@ -206,18 +207,22 @@ class testapis(Resource):
                     jsonobject = jsonobject[:-1]+'}]}'
                     return json.loads(jsonobject)
 
-######################
-### api references ###
-######################
+########################
+#### api references ####
+########################
+
 api.add_resource(genApiFetch,
-    '/fetch/<string:kspace>/<string:thing>',
-    '/fetch/<string:kspace>/<string:thing>/<string:has>',
-    '/fetch/<string:kspace>/<string:thing>/<int:limit>',
-    '/fetch/<string:kspace>/<string:thing>/<string:has>/<string:get>',
-    '/fetch/<string:kspace>/<string:thing>/<string:has>/<int:limit>',
-    '/fetch/<string:kspace>/<string:thing>/<string:has>/<string:get>/<int:limit>',)
-api.add_resource(testapis, '/test')
-api.add_resource(ApiPing, '/ping')
+    '/fetch/<string:kspace>/<string:thing>',                                        #### http://127.0.0.1:5000/fetch/<keyspace_name>/<thingtype=thingname>
+    '/fetch/<string:kspace>/<string:thing>/<string:has>',                           #### http://127.0.0.1:5000/fetch/<keyspace_name>/<thingtype=thingname>/<parametername=parametervalue>
+    '/fetch/<string:kspace>/<string:thing>/<int:limit>',                            #### http://127.0.0.1:5000/fetch/<keyspace_name>/<thingtype=thingname>/<limitvalue>
+    '/fetch/<string:kspace>/<string:thing>/<string:get>',                           #### http://127.0.0.1:5000/fetch/<keyspace_name>/<thingtype=thingname>/<getvariables>
+    '/fetch/<string:kspace>/<string:thing>/<string:has>/<string:get>',              #### http://127.0.0.1:5000/fetch/<keyspace_name>/<thingtype=thingname>/<parametername=parametervalue>/<getvariables>
+    '/fetch/<string:kspace>/<string:thing>/<string:has>/<int:limit>',               #### http://127.0.0.1:5000/fetch/<keyspace_name>/<thingtype=thingname>/<parametername=parametervalue>/<limitvalue>
+    '/fetch/<string:kspace>/<string:thing>/<string:has>/<string:get>/<int:limit>',) #### http://127.0.0.1:5000/fetch/<keyspace_name>/<thingtype=thingname>/<parametername=parametervalue>/<getvariable>/<limitvalue>
+
+api.add_resource(testapis, '/test') #### http://127.0.0.1:5000/test
+
+api.add_resource(ApiPing, '/ping') #### http://127.0.0.1:5000/ping
 
 if __name__ == '__main__':
    app.run(debug=True)
